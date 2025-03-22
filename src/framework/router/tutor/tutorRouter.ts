74e809707f370bb0c509e91981cc261b1d0a87da
karnan <karnan.se@gmail.com>
2025-03-22 17:04:20 +0530

    going to add checker middleware

--- src/framework/router/tutor/tutorRouter.ts
+++ src/framework/router/tutor/tutorRouter.ts
@@ -9,8 +9,22 @@ import { courseController } from "../../dependencies/course/course"
 import { uploadVideo_thumbnail } from "../../../adapters/middleware/multervideo"
 import { conversationController } from "../../dependencies/conversation/conversationDependency"
 import { revenueController } from "../../dependencies/revenue/revenueDependency"
+import { Request , Response , NextFunction } from "express"
 
 const router = express.Router()
+
+const checker =(req :Request, res:Response, next:NextFunction)=>{
+    try {
+        console.log(req.body , "checks the requeest")
+        next()
+
+        
+    } catch (error) {
+        console.log(error);
+        throw error
+        
+    }
+}
 
 
 router.post("/tutorRegister",  (req, res, next)=>tutorController.Signup(req, res, next))
@@ -19,7 +33,7 @@ router.post("/resendOTP",   (req, res, next)=>tutorVerifyController.resendOTP(re
 router.post("/verifyUser",   (req, res, next)=> tutorVerifyController.verifyUser(req, res, next))
 router.post("/applicationForm", uploadFile, jwtAuth, (req, res, next)=>tutorApplicationController.TutorApplication(req, res, next))
 router.post("/update", jwtAuth, (req, res, next)=> tutorController.updatedUser(req, res,next))
-router.post("/createCourse", uploadVideo_thumbnail, jwtAuth, (req, res, next)=>courseController.newCourse(req, res, next))
+router.post("/createCourse", checker ,  uploadVideo_thumbnail,  (req, res, next)=>courseController.newCourse(req, res, next))
 router.get("/getallCourse",jwtAuth, (req, res, next)=>courseController.getAllCourse(req, res, next))
 router.put("/updateSection", jwtAuth, uploadVideo_thumbnail , (req, res, next)=> courseController.updateSection(req, res, next))
 router.post("/getCourse", jwtAuth ,(req, res, next)=>courseController.getCourse(req, res, next))
